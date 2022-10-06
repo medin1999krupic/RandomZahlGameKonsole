dc1e5ed4df0a07095f320027afb68a99891c91d5
m.krupic <mkrupic@rocongruppe.de>
2022-10-06 18:57:54 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateSpielConsole
+{
+    class Program
+    {
+        public static int GeneratedNumber { get; set; }
+        public static int Versuche { get; set; }
+        public static bool GameStarted { get; set; }
+        public static bool Gewonnen { get; set; }
+
+        static void Main(string[] args)
+        {
+            //String start = "START";
+
+
+            ShowCommands();
+            string frage = "";
+            while((frage = Console.ReadLine().ToUpper()) != "EXIT")
+            {
+                if (frage.ToUpper() == "START")
+                {
+                    if(!GameStarted)
+                        StartGame();
+
+                    while(GameStarted)
+                    {
+                        Console.WriteLine("Gebe eine Zahl ein von 1 - 100.");
+                        string eigegebeneZahlstr = Console.ReadLine();
+                        int zahl;
+                        bool isValidNumber = int.TryParse(eigegebeneZahlstr, out zahl);
+                        if(isValidNumber)
+                        {
+                            ErgebnisAuswerten(zahl);
+                            Versuche = Versuche - 1;
+                        }
+                           
+                        else
+                        {
+                            
+                            if (eigegebeneZahlstr.ToUpper() == "EXIT")
+                            {
+                                ExitGame();
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Bitte geben Sie eine gültige Zahl ein.");
+                            }
+                        }
+
+                        if(Versuche == 0 && !Gewonnen)
+                        {
+                            Console.WriteLine("Du hast verloren.");
+                            GameStarted = false;
+                            ShowCommands();
+                        }
+
+                        if(Gewonnen)
+                        {
+                            ShowCommands();
+                        }
+                    }
+                }
+                else
+                {
+                    ShowCommands();
+                }
+
+            }
+
+            ExitGame();
+            
+
+        }
+
+        private static void ShowCommands()
+        {
+            Console.WriteLine("Starte das Spiel indem du START schreibst.");
+            Console.WriteLine("Drücke EXIT um das Spiel zu Beenden.");
+        }
+
+        private static void ExitGame()
+        {
+            Console.WriteLine("Das Spiel wird beendet!!!");
+            Console.ReadLine();
+        }
+
+        private static void ErgebnisAuswerten(int zahl)
+        {
+            if (zahl == GeneratedNumber)
+            {
+                Console.WriteLine("Gewonnen");
+                GameStarted = false;
+                Gewonnen = true;
+            }
+            else if (zahl > GeneratedNumber)
+            {
+                Console.WriteLine("Zahl ist zu groß");
+            }
+            else
+            {
+                Console.WriteLine("Zahl ist zu klein");
+            }
+        }
+
+        private static void StartGame()
+        {
+            //beginnt das spiel
+            Console.WriteLine("Du bist im Spiel.");
+            //string frage1 = Console.ReadLine();
+            Console.WriteLine("Zahl wird Generiert ...");
+            int num = new Random().Next(0, 100);
+            GeneratedNumber = num;
+            Versuche = 3;
+            GameStarted = true;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("RateSpielConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RateSpielConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("636d33c7-b60d-4569-b97d-5d0e9b269502")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
